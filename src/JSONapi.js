2889ac5445c40e9cb85e5b5ec949ab955505edcc
deniscodes <piroko25>
2021-04-10 20:29:57 -0400

    implementation : database connection

--- src/JSONapi.js
+++ src/JSONapi.js
@@ -1,1 +1,14 @@
 import axios from 'axios';
+
+const openDataBase = axios.create({
+    baseURL:'https://my-json-server.typicode.com/DenisCodes/database'
+});
+
+const getDataBaseById = async (ID) => {
+    const response = await openDataBase.get('/tasks/'+ String(ID));
+    return response;
+}
+
+export default{
+    openDataBase, getDataBaseById
+}
