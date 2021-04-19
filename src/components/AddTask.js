cb1a1eb4309316401cf5373025bfb8e0534f1409
ahedhli21 <55544100+nourahedhli@users.noreply.github.com>
2021-04-19 17:08:50 -0400

    adding task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,66 +1,56 @@
 import React from 'react';
+import axios from "axios";
 
 
 
 class AddTask extends React.Component {
-     state = { newTask: ""}
 
-// we ned to add this to the database
-    // probably I can do it
     addNewTask (newTask) {
         console.log(newTask);
 
     }
-    onFormSubmit = (e) => {
+// we ned to add this to the database
+    // probably I can do it
+
+    onSubmit = (e) => {
         const newTask ={
             title : this.refs.title.value,
             type : this.refs.type.value
-
         }
-
-
+        this.addNewTask(newTask);
+        this.props.onSubmit(this.state.addNewTask(newTask));
         e.preventDefault();
 
-        this.props.onSubmit(this.state.addNewTask(newTask))
-
-
     }
-
     render(){
 
-       return (
+        return (
 
-           <div>
-               <form className="task-input form-group" onSubmit={this.onFormSubmit}>
-                   <h2>Add Task</h2>
-                   <label htmlFor="title">Enter New Task</label>
-                   <input type="text" className="form-control"
-                          name="title"
-                          ref="title"
+            <div>
+                <form className="task-input form-group" onSubmit={this.onSubmit.bind(this)}>
+                    <h2>Add Task</h2>
+                    <label htmlFor="title">Enter New Task</label>
+                    <input type="text" className="form-control"
+                           name="title"
+                           ref="title"
+                    />
 
-                          />
+                    <label htmlFor="type">Type</label>
+                    <select className="browser-default custom-select" name="type" ref="type">
+                        <option>Choose your option</option>
+                        <option value="task">Task</option>
+                        <option value="debug">Debug</option>
+                        <option value="feature">Feature</option>
+                    </select>
+                    <input type="submit" value="Add Task" className="btn btn-primary"/>
+                </form>
 
-                   <label htmlFor="type">Type</label>
-                   <select className="browser-default custom-select"
-                           name="type"
+            </div>
+        );
 
-                           ref="type"
-                           >
-                       <option>Choose your option</option>
-                       <option value="task">Task</option>
-                       <option value="debug">Debug</option>
-                       <option value="feature">Feature</option>
-                   </select>
-                   <input type="submit" value="Add Task" className="btn btn-primary"/>
-               </form>
-
-           </div>
-       );
-
-   }
+    }
 
 }
 
 
 export default AddTask;
-
