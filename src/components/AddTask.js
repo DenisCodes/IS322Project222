8e5cbd737926d2ac4739a06d075f51e8b89210ac
ahedhli21 <55544100+nourahedhli@users.noreply.github.com>
2021-04-19 16:54:28 -0400

    adding task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -5,23 +5,24 @@ import React from 'react';
 class AddTask extends React.Component {
      state = { newTask: ""}
 
-  addNewTask = (newTask)=> {
-       console.log(newTask);
-
-   }
 // we ned to add this to the database
     // probably I can do it
+    addNewTask (newTask) {
+        console.log(newTask);
 
-    onSubmit = (e) => {
+    }
+    onFormSubmit = (e) => {
         const newTask ={
             title : this.refs.title.value,
             type : this.refs.type.value
+
         }
-        this.addNewTask(newTask);
+
+
         e.preventDefault();
 
-        this.props.onSubmit(this.state.newTask)
-        this.setState({newTask: ""})
+        this.props.onSubmit(this.state.addNewTask(newTask))
+
 
     }
 
@@ -30,16 +31,21 @@ class AddTask extends React.Component {
        return (
 
            <div>
-               <form className="task-input form-group" onSubmit={this.onSubmit.bind(this)}>
+               <form className="task-input form-group" onSubmit={this.onFormSubmit}>
                    <h2>Add Task</h2>
                    <label htmlFor="title">Enter New Task</label>
                    <input type="text" className="form-control"
                           name="title"
                           ref="title"
+
                           />
 
                    <label htmlFor="type">Type</label>
-                   <select className="browser-default custom-select" name="type" ref="type">
+                   <select className="browser-default custom-select"
+                           name="type"
+
+                           ref="type"
+                           >
                        <option>Choose your option</option>
                        <option value="task">Task</option>
                        <option value="debug">Debug</option>
